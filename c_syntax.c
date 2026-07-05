1d8b62d136b9820817930662ca6461f101e61ece
Vimalathas Vithusha <156227942+vvithusha@users.noreply.github.com>
2026-07-05 08:28:36 +0530

    multiple variables

---
+++ c_syntax.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    printf("hello world\n");
+
+    //new line statement
+    printf("hello\nvithusha");
+
+    //variable declaration
+    char name[] = "vithu";
+    int age = 20;
+    printf("\nhello %s your age is %d",name,age);
+
+    //multiple variable declaration
+    char name1[] = "vithu";   
+    int age1= 20;
+    printf("\nhello %s your age is %d",name1,age1);
+
+    
+
+    return 0;
+}
