7aed75bcfc4ab7ad95a936acbd24487444989576
Vimalathas Vithusha <156227942+vvithusha@users.noreply.github.com>
2026-07-06 07:38:16 +0530

    different types of data types

--- data_type.c
+++ data_type.c
@@ -14,5 +14,32 @@ int main()
     printf("two_decimal_float : %.2f\n",fnum);
     printf("three_decimal_double : %.3lf\n",dnum);
 
+
+    //get the memory size 
+    printf("size of int : %zu\n",sizeof(num));
+    printf("size of float : %zu\n",sizeof(fnum));
+    printf("size of double : %zu\n",sizeof(dnum));
+    printf("size of char : %zu\n",sizeof(ch));
+
+    int normalInt = 1000;                       // standard int 
+    double normalDouble = 3.14;                 // standard double
+
+    short int small = -100;                     // smaller int
+    unsigned int count = 25;                    // only positive int
+    long int big = 1234567890;                  // larger int
+    long long int veryBig = 9223372036854775807; // very large int
+    unsigned long long int huge = 18446744073709551615U; // very large, only positive
+    long double precise = 3.141592653589793238L; // extended precision
+
+    printf("Normal int: %d\n", normalInt);
+    printf("Normal double: %lf\n", normalDouble);
+    printf("Small: %hd\n", small);
+    printf("Count: %u\n", count);
+    printf("Big: %ld\n", big);
+    printf("Very Big: %lld\n", veryBig);
+    printf("Huge: %llu\n", huge);
+    printf("Precise: %Lf\n", precise);
+
+    
     return 0;
 }
